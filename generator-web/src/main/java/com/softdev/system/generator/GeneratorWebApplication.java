eb7760ac18139088bdbdfe110d25e242f9257c67
gaohanghang <1341947277@qq.com>
2019-12-27 14:43:49 +0800

    动态获取tomcat启动端口，控制台打印项目访问地址

--- generator-web/src/main/java/com/softdev/system/generator/GeneratorWebApplication.java
+++ generator-web/src/main/java/com/softdev/system/generator/GeneratorWebApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class GeneratorWebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeneratorWebApplication.class,args);
-		log.info("项目启动启动成功！访问地址: http://localhost:1234/generator");
 	}
 }
