781a7ff4caa4c21bc9d2d8554d418a7ac1c3df69
Moshow郑锴 <moshowgame@126.com>
2022-07-02 17:54:12 +0800

    script to install and run

---
+++ @install.cmd
@@ -0,0 +1,5 @@
+REM JDK must equal or more than : 11
+java -version
+REM maven installing libraries
+mvn clean compile
+pause
---
+++ @run.cmd
@@ -0,0 +1,4 @@
+java -jar "./generator-web/target/generator-web-3.0.jar"
+pause
+mvn clean compile package
+pause
