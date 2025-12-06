abaaa409655e59026a17400ee3c1418bfe8562e7
Moshow郑锴 <moshowgame@126.com>
2025-12-07 00:03:29 +0800

    fix GlobalDefaultExceptionHandler

--- generator-web/src/main/java/com/softdev/system/generator/config/GlobalDefaultExceptionHandler.java
+++ generator-web/src/main/java/com/softdev/system/generator/config/GlobalDefaultExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.softdev.system.generator.config;
 
-import com.softdev.system.generator.entity.ReturnT;
+import com.softdev.system.generator.entity.vo.ResultVo;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * @author zhengkai.blog.csdn.net
@@ -15,9 +14,9 @@ public class GlobalDefaultExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public ReturnT defaultExceptionHandler(HttpServletRequest req, Exception e) {
+    public ResultVo defaultExceptionHandler(HttpServletRequest req, Exception e) {
         e.printStackTrace();
-        return ReturnT.error("代码生成失败:"+e.getMessage());
+        return ResultVo.error("代码生成失败:"+e.getMessage());
     }
 
 }
